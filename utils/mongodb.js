612e15d426890bcb5c1cf1a442766dd81c2f163b
VolkanIpr2 <volkan_751@hotmail.de>
2024-06-20 17:09:38 +0200

    Update mongodb.js

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,39 +1,39 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-let cached = global.mongoose
+let cached = global.mongoose;
 
-if(!cached) {
-    cached = global.mongoose = { con: null, promise: null}
+if (!cached) {
+    cached = global.mongoose = { con: null, promise: null };
 }
 
-async function dbConnect () {
-    if (cached.con){
-        //console.log('DB Verbindung aktiv')
-        return cached.con
+async function dbConnect() {
+    if (cached.con) {
+        // console.log('DB Verbindung aktiv');
+        return cached.con;
     }
 
     if (!cached.promise) {
         const options = {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             bufferCommands: false,
-        }
+            // Weitere Optionen bei Bedarf hinzufügen
+        };
 
-        cached.promise = mongoose.connect(process.env.MONGODB_URI, options).then(mongoose =>{
-            //console.log('DB Verbindung gestartet')
-            return mongoose
-        })
-
-        
+        cached.promise = mongoose.connect(process.env.MONGODB_URI, options).then((mongoose) => {
+            // console.log('DB Verbindung gestartet');
+            return mongoose;
+        });
     }
-    cached.con = await cached.promise
-    return cached.con
+    cached.con = await cached.promise;
+    return cached.con;
 }
 
-async function dbDisconnect(){
-    await mongoose.disconnect();
-    //console.log('DB Verbindung beendet')
+async function dbDisconnect() {
+    if (cached.con) {
+        await mongoose.disconnect();
+        cached.con = null;
+        // console.log('DB Verbindung beendet');
+    }
 }
 
-const mongodb = {dbConnect, dbDisconnect}
-export default mongodb 
+const mongodb = { dbConnect, dbDisconnect };
+export default mongodb;
